46d83842a00ec6c7aa7fccad46a70cc62645fe80
Arfaniel <dn120284aio@gmail.com>
2016-10-06 12:07:17 +0300

    foreach works

--- Products/Program.cs
+++ Products/Program.cs
@@ -13,11 +13,21 @@ namespace Products
         
         static void Main(string[] args)
         {
-
-            Product one = new Product();
-            one.expired = DateTime.Now.AddDays(11);
-            one.Examine();
-            one.Show();
+            Product[] one = new Product[5];
+            for (int i = 0; i < one.Length; i++)
+            {
+                one[i] = new Product();
+            }
+            one[0].expired = DateTime.Now.AddDays(11);
+            one[1].expired = DateTime.Now.AddDays(1);
+            one[2].expired = DateTime.Now;
+            one[3].expired = DateTime.Now.AddDays(-1);
+            one[4].expired = DateTime.Now.AddDays(-5);
+            foreach (Product item in one)
+            {
+                item.Examine();
+                item.Show();
+            }
         }
     }
 }
