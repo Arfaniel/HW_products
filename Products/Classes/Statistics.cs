45aacc53d66a5b05a21bed7cb51d3da5aabc476c
Arfaniel <dn120284aio@gmail.com>
2016-10-06 15:38:42 +0300

    statistics class

--- Products/Classes/Statistics.cs
+++ Products/Classes/Statistics.cs
@@ -8,7 +8,60 @@ namespace Products.Classes
 {
     public class Statistics
     {
+        public delegate int counter();
+        
 
+        public void TotalSum(Product[] one)
+        {
+            float sum = one[0].price;
+            for (int i = 1; i < one.Length; i++)
+            {
+                sum += one[i].price;
+            }
+            Console.WriteLine($"Total price is: {sum}");
+        }
+        public void SumExp(Product[] one)
+        {
+            float sum = 0;
+            DateTime now = DateTime.Now;
+            for (int i = 1; i < one.Length; i++)
+            {
+                if (one[i].expired <= now)
+                {
+                    sum += one[i].price;
+                }
+            }
+            Console.WriteLine($"Total price for all expired prods is: {sum}"); 
+        }
+        public void avgPrice(Product[] one)
+        {
+            float sum = 0;
+            int i = 0;
+            for (; i < one.Length; i++)
+            {
+                sum += one[i].price;
+            }
+            sum = sum / i;
+            Console.WriteLine($"Average price is: {sum}" );
+        }
+        public void TotalWeight(Product[] one)
+        {
+            float sum = 0;
+            for (int i = 1; i < one.Length; i++)
+            {
+                sum += one[i].weight;
+            }
+            Console.WriteLine($"Total weight is {sum}");
+        }
+        public void Menu()
+        {
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            counter expCount = null;
+            do
+            {
+                Console.WriteLine();
+            } while (key.Key != ConsoleKey.Escape);
+        }
 
     }
 }
